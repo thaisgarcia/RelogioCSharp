745280e018e9e6e7d16a355a4de33030d371424c
Thais Garcia <thaisgarcia.t11@gmail.com>
2022-03-26 17:36:10 -0300

    first commit

---
+++ Relógio/Relógio/FrmRelógio.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Relógio
+{
+    partial class FrmRelógio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblDate = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblRelogio = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.SystemColors.Window;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblDate});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 353);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(739, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblDate
+            // 
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(13, 17);
+            this.lblDate.Text = "0";
+            this.lblDate.Click += new System.EventHandler(this.lblRelogio_Click);
+            // 
+            // lblRelogio
+            // 
+            this.lblRelogio.AutoSize = true;
+            this.lblRelogio.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRelogio.Location = new System.Drawing.Point(293, 141);
+            this.lblRelogio.Name = "lblRelogio";
+            this.lblRelogio.Size = new System.Drawing.Size(31, 33);
+            this.lblRelogio.TabIndex = 1;
+            this.lblRelogio.Text = "0";
+            // 
+            // FrmRelógio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(739, 375);
+            this.Controls.Add(this.lblRelogio);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "FrmRelógio";
+            this.Text = "Relógio";
+            this.Load += new System.EventHandler(this.FrmRelógio_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblDate;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblRelogio;
+    }
+}
+
---
+++ Relógio/Relógio/FrmRelógio.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Relógio
+{
+    public partial class FrmRelógio : Form
+    {
+        public FrmRelógio()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmRelógio_Load(object sender, EventArgs e)
+        {
+            lblDate.Text = DateTime.Now.ToLongDateString();
+            lblRelogio.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblRelogio.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void lblRelogio_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
